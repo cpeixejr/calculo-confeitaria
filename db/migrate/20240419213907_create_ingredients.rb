b122c60c39778d6b6e21724fff75e6ce23a7eae9
Carlos Peixoto Mangueira Junior <carlos.peixoto@MacBook-Pro-de-Carlos.local>
2024-04-19 23:22:30 -0300

    feat(ingredient.rb): Ingredient model creator

---
+++ db/migrate/20240419213907_create_ingredients.rb
@@ -0,0 +1,10 @@
+class CreateIngredients < ActiveRecord::Migration[7.1]
+  def change
+    create_table :ingredients do |t|
+      t.string :name, null: false
+      t.float :price, null: false
+
+      t.timestamps
+    end
+  end
+end
