e2e22a511e19c4f897689bae9fcb8d299dfcd684
Carlos Peixoto Mangueira Junior <cpeixejr@gmail.com>
2024-04-23 21:35:54 -0300

    feat(ingredient.rb): Add broadcast turbo frame to Ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,7 @@
 class Ingredient < ApplicationRecord
   validates :name, :price, presence: true
 
+  broadcasts_to ->(ingredient) { "ingredients" }, inserts_by: :prepend
+
   scope :ordered, -> { order(id: :desc) }
 end
