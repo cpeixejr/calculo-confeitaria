5b009c8e7aa5b7d2e7e75f38e30eaa5db500086c
Carlos Peixoto Mangueira Junior <cpeixejr@gmail.com>
2024-06-11 22:14:46 -0300

    feat: Add flash messages notifications

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -17,31 +17,45 @@ class IngredientsController < ApplicationController
     @ingredient = current_user.ingredients.build(ingredient_params)
 
     if @ingredient.save
+      flash.now[:notice] = 'Ingredient was successfully created.'
+
       respond_to do |format|
-        format.html { redirect_to ingredients_path, notice: 'Ingredient was successfully created.' }
+        format.html { redirect_to ingredients_path }
         format.turbo_stream
       end
     else
+      flash.now[:alert] = 'Failed to create the Ingredient.'
       render :new, status: :unprocessable_entity
     end
+
   end
 
   def edit; end
 
   def update
     if @ingredient.update(ingredient_params)
-      redirect_to ingredients_path, notice: 'Ingredient was successfully updated.'
+      flash.now[:notice] = 'Ingredient was successfully updated.'
+
+      respond_to do |format|
+        format.html { redirect_to ingredients_path }
+        format.turbo_stream
+      end
     else
+      flash.now[:alert] = 'Failed to update the Ingredient.'
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @ingredient.destroy
+    if @ingredient.destroy
+      flash.now[:notice] = 'Ingredient was successfully destroyed.'
 
-    respond_to do |format|
-      format.html { redirect_to ingredients_path, notice: 'Ingredient was successfully destroyed.' }
-      format.turbo_stream
+      respond_to do |format|
+        format.html { redirect_to ingredients_path }
+        format.turbo_stream
+      end
+    else
+      flash.now[:alert] = 'Failed to destroy the Ingredient.'
     end
   end
 
